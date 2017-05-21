8bc2c968afdba676051c48aebc6d043eb963c3b2
Liroo <pierre-monge@hotmail.fr>
2017-05-21 15:06:07 +0200

    remove request_send file empty

--- ftp_server/src/request/request_send.c
+++
@@ -1,9 +0,0 @@
-/*
-** request_send.c for request in /Users/pierre/Epitech/PSU/FTP/ftp_server
-**
-** Made by Pierre Monge
-** Login   <pierre@epitech.net>
-**
-** Started on  Sat May 20 06:43:01 2017 Pierre Monge
-** Last update Sun May 21 04:21:58 2017 Pierre Monge
-*/
